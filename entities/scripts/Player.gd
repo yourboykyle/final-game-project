8fa469c8fdb4eb86d0d872959f2eba4af88fb595
jow555 <jasperkaddatz@gmail.com>
2026-03-12 19:49:17 -0400

    Added sprinting, added player "death"
    Nothing actually happens for player death except a print but now we have the conditions for it

--- entities/scripts/Player.gd
+++ entities/scripts/Player.gd
@@ -2,8 +2,12 @@ extends CharacterBody2D
 
 signal oxygen_changed(current_oxygen: float, max_oxygen: float)
 
+# Life variables
 @export var max_oxygen := 100.0
 var oxygen := max_oxygen
+@onready var death_timer: Timer = $DeathTimer
+var can_die = true
+#Life variables end
 
 # Weapon Varaibles Start
 # Gets the players weapon holder
@@ -14,6 +18,7 @@ var current_weapon
 @onready var sprite_2d: Sprite2D = $Sprite2D
 # Movement variables
 @export var SPEED = 1000
+var speed_multiplier = 1
 @onready var dive_time: Timer = $DiveTime
 var diving : bool = false
 # Movement variables end
@@ -39,41 +44,67 @@ func _physics_process(delta):
 		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	)
 	
-	velocity = input_vector.normalized() * SPEED
+	if Input.is_action_pressed("boost"):
+		#Multiplier for movement
+		speed_multiplier = 1.5
+	else:
+		speed_multiplier = 1
+	
+	velocity = input_vector.normalized() * SPEED * speed_multiplier
 	move_and_slide()
 	
 	
 	if Input.is_action_just_pressed("dive"):
 		dive()
+	
+	
 
 
 func _update_oxygen(delta):
-	var decay = Globals.oxygen_decay_rate * delta
+	var decay = Globals.oxygen_decay_rate * delta * speed_multiplier #decays faster while boosting
 	oxygen = max(oxygen -decay, 0);
 	emit_signal("oxygen_changed", oxygen, max_oxygen)
+	
+	# Check oxygen levels
+	if oxygen <= 0:
+		#Start death if the player can die
+		death(can_die)
 
 func take_damage(amount):
+	
 	
 	if diving:
 		print("missed")
 		return
 	
-	""" put in the oxygen stuff later
-	health-= amount
-	health_bar.value = health 
-	if health <= 0: 
-		queue_free() 
-	"""
+	
+	oxygen -= amount
+	emit_signal("oxygen_changed", oxygen, max_oxygen)
+	
+	if oxygen <= 0:
+		death(can_die)
 	
 	print("ouch")
 
 
 func dive():
 	diving = true
-	
+	take_damage(1)
 	dive_time.start()
 
+func death(can_die):
+	#Check if they can die, if they can, set can die to false and start 10 seconds till super death
+	if can_die:
+		self.can_die = false
+		print("10 SECONDS UNTIL UNCONCIOUS")
+		death_timer.start()
+	
+	return
 
 func _on_dive_time_timeout() -> void:
 	
 	diving = false
+
+
+func _on_death_timer_timeout() -> void:
+	print("died")
