964b4147957863bdbd8f5873d4c5cd7ff28c68d1
Zachary Seel <starwarsimposter@gmail.com>
2026-03-23 12:53:43 -0400

    Rudimentary Enemy Stop
    Stops enemies from colliding with player.
    Very Basic

--- entities/scripts/Enemy.gd
+++ entities/scripts/Enemy.gd
@@ -7,6 +7,7 @@ var SPEED = 300
 var health = 100 
 var max_health = 100 
 var enemy_id = ""
+var stop_distance = 150
 
 @onready var health_bar = $HealthBar
 
@@ -15,10 +16,14 @@ func _ready():
 	health_bar.max_value = max_health 
 	health_bar.value = health 
 func _physics_process(delta: float) -> void: 
-	agent.target_position = player.global_position 
-	var next_point = agent.get_next_path_position() 
-	var direction = (next_point - global_position).normalized() 
-	velocity = direction * SPEED 
+	agent.target_position = (player.global_position) 
+	var distance = global_position.distance_to(player.global_position)
+	if distance > stop_distance:
+		var next_point = agent.get_next_path_position() 
+		var direction = (next_point - global_position).normalized() 
+		velocity = direction * SPEED
+	else: 
+		velocity = Vector2.ZERO
 	move_and_slide()
 
  
