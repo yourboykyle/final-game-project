24e53c22a7784a2a3d6912b3f2bdfbe8904b0700
jow555 <jasperkaddatz@gmail.com>
2026-04-02 23:56:03 -0400

    fixed looping on export

--- rooms/scripts/CombatRoom.gd
+++ rooms/scripts/CombatRoom.gd
@@ -162,4 +162,4 @@ func _on_enemy_defeated(enemy_position):
 		Globals.room_pickups[room_id].append({"type": "chest", "position": Vector2(enemy_position.x, enemy_position.y)})
 	
 	create_bubble(enemy_position.x + 128, enemy_position.y)
-	Globals.room_pickups[room_id].append({"type": "bubble", "position": Vector2(enemy_position.x+256, enemy_position.y)}) 
+	Globals.room_pickups[room_id].append({"type": "bubble", "position": Vector2(enemy_position.x+128, enemy_position.y)}) 
