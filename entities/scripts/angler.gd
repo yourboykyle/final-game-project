4edd9a00956f73fd3659446d3383f3496b6c2eec
Zachary Seel <starwarsimposter@gmail.com>
2026-04-03 21:46:38 -0400

    Angler Bite Attack
    Teleports under the player, biting them

--- entities/scripts/angler.gd
+++ entities/scripts/angler.gd
@@ -15,7 +15,12 @@ var dash_time = 2
 var dash_speed = 500
 var dash_direction 
 var dash_cooldown = 10 
-var has_hit_player = false
+var has_hit_player = false 
+var bite_cooldown = 20
+var bite_timer = 5
+var bite_end = 1 
+var has_spawned = false 
+var flipped = true
 func _ready(): 
 	attack_timer = 2.0
 	max_health = 1000 
@@ -29,12 +34,21 @@ func _physics_process(delta:float):
 	match state: 
 		State.IDLE: 
 			cooldown -= delta 
-			velocity = Vector2i.ZERO
+			velocity = Vector2i.ZERO 
+			$Sprite2D.rotation = 0
 			if cooldown <= 0: 
 				state = State.CHASE 
+				dash_cooldown = 10
 		State.CHASE: 
 			chase_player() 
-			dash_cooldown -= delta
+			dash_cooldown -= delta 
+			bite_cooldown -= delta 
+			if bite_cooldown <= 0: 
+				state = State.BITE 
+				bite_timer = 5
+				bite_end = 1 
+				has_spawned = false
+				return
 			var distance = global_position.distance_to(player.global_position) 
 			if distance <= 500 and dash_cooldown <= 0:
 				var dir = player.global_position - global_position 
@@ -56,10 +70,33 @@ func _physics_process(delta:float):
 		State.ULTIMATE: 
 			print("ULTIMATE")
 		State.BITE: 
-			print("teleporting under and biting")
+			bite_timer -= delta
+			if bite_timer > 0:
+				hide()
+				$CollisionShape2D.set_disabled(true) 
+				velocity = Vector2.ZERO
+
+			else:
+				if not has_spawned:
+					show() 
+					print("showing")
+					global_position = player.global_position + Vector2(0, 250)
+					sprite_2d.flip_h = false
+					$Sprite2D.rotation = (player.global_position - global_position).angle() + deg_to_rad(180)
+					velocity = Vector2(0, -1) * speed 
+					has_spawned = true
+				bite_end -= delta
+				if bite_end <= 0:
+					state = State.IDLE 
+					bite_cooldown = 20
+					has_spawned = false
+					velocity = Vector2i.ZERO 
+					rotation = velocity.angle()
+					$CollisionShape2D.set_deferred("disabled", false)
+			
 			
 	
-	if velocity.x != 0:
+	if velocity.x != 0 and state != State.BITE:
 		sprite_2d.flip_h = velocity.x > 0
 	
 	move_and_slide()
@@ -77,10 +114,15 @@ func chase_player():
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player") and state == State.DASH: 
-		Globals.player.take_damage(10) 
+	if body.is_in_group("player") and (state == State.DASH or state == State.BITE) and visible: 
+		if state == State.BITE: 
+			Globals.player.take_damage(30) 
+			bite_cooldown = 10 
+			$CollisionShape2D.set_deferred("disabled", false)
+		else: 
+			Globals.player.take_damage(10) 
+			dash_cooldown = 10  
 		has_hit_player = true 
-		state = State.IDLE 
-		dash_cooldown = 10 
+		state = State.IDLE
 		cooldown = 1 
 		print("get bit") # Replace with function body.
